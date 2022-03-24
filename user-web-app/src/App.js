82525ad8f85f03bafbbf1364b0062495c2e38a31
MeghaWaje <megha14.waje@gmail.com>
2022-03-24 23:38:58 +0530

    added spinner and navbar

--- user-web-app/src/App.js
+++ user-web-app/src/App.js
@@ -1,53 +1,61 @@
-import React, { useState } from 'react';
-//import { Card, Button, CardImg, CardTitle, CardText, CardDeck, CardSubtitle, CardBody } from 'reactstrap';
-import { Button, Card, Row, Col } from 'react-bootstrap';
-import './App.css';
+import React, { useState } from "react";
+import { Card, Row, Col } from "react-bootstrap";
+import { Navbar, Container } from "react-bootstrap";
+import { TailSpin } from "react-loader-spinner";
+import "./App.css";
 
 const App = () => {
   const [users, setUsers] = useState([]);
+  const [spinnerLoading, setSpinnerLoading] = useState(true);
 
   const loadUsers = async () => {
-    console.log('before');
     const response = await fetch("https://reqres.in/api/users?page=1");
-
     const jsonresponse = await response.json();
-    console.log('after', jsonresponse.data)
-
     setUsers(jsonresponse.data);
+    setSpinnerLoading(false);
   };
 
-
   return (
-    <div className='App'>
-      <h1>hello all</h1>
-      <button onClick={loadUsers}>Get Users</button>
-
-
-
-      <h2>Users</h2>
+    <div className="App">
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Users</Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              <button onClick={loadUsers}>Get Users</button>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <Row>
-
-
         {users.map(({ id, first_name, last_name, avatar, email }) => (
-
           <Col key={id} md={4}>
-            <Card >
+            <Card style={{ margin: "20px" }}>
               <Card.Img variant="top" src={avatar} />
               <Card.Body>
-                <Card.Title>{first_name} {last_name} </Card.Title>
+                <Card.Title>
+                  {first_name} {last_name}{" "}
+                </Card.Title>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up the bulk of
-                  the card's content.
+                  E-mail: <a href={"mailto:" + email}>{email}</a>
                 </Card.Text>
-                <Button variant="primary">{email}</Button>
               </Card.Body>
             </Card>
           </Col>
-          // <li key={id}> name: {first_name} {last_name} Email:{email} </li>
-
         ))}
       </Row>
+      {spinnerLoading ? (
+        <div className="centered">
+          <TailSpin
+            visible={spinnerLoading}
+            color="#00BFFF"
+            height={100}
+            width={100}
+          />
+        </div>
+      ) : null}
     </div>
-  )
-}
+  );
+};
 export default App;
